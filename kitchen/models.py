5f16457b277470fb62f486d01243c9d57dc3fecc
Timur Khvat <timurkhvat@gmail.com>
2025-05-08 17:00:53 +0200

    Add DishType, Cook, Ingredient, Dish models with admin registration

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name}) exp.{self.years_of_experience}"
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} price: {self.price} dish type: {self.dish_type.name}"
+
+
+
