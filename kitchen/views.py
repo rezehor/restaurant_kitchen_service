398b12347dec435432de56b602faea3d04db8a45
Timur Khvat <timurkhvat@gmail.com>
2025-05-08 18:46:26 +0200

    add url, view and template for home page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import DishType, Dish, Cook, Ingredient
+
+
+def index(request):
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+    num_ingredients = Ingredient.objects.count()
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+        "num_ingredients": num_ingredients,
+    }
+    return render(request, "kitchen/index.html", context=context)
