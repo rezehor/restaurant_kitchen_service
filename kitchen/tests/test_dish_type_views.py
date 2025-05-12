c7aac9a933a2a138add95292993ee203e6eb08b2
Timur Khvat <timurkhvat@gmail.com>
2025-05-12 10:35:48 +0200

    add tests for views

---
+++ kitchen/tests/test_dish_type_views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from kitchen.models import DishType
+
+DISH_TYPE_URL = reverse("kitchen:dish-types-list")
+
+class PublicDishTypeTest(TestCase):
+    def test_login_required(self):
+        res = self.client.get(DISH_TYPE_URL)
+        self.assertNotEqual(res.status_code, 200)
+
+
+class PrivateDishTypeTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="<PASSWORD>",
+        )
+        self.client.force_login(self.user)
+
+    def test_retrieve_dish_type(self):
+        DishType.objects.create(name="Breakfast")
+        DishType.objects.create(name="Dessert")
+        response = self.client.get(DISH_TYPE_URL)
+        self.assertEqual(response.status_code, 200)
+        dish_type = DishType.objects.all()
+        self.assertEqual(
+            list(response.context["dish_type_list"]),
+            list(dish_type),
+        )
+        self.assertTemplateUsed(response, "kitchen/dish_type_list.html")
---
+++ kitchen/tests/test_models.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from kitchen.models import DishType, Ingredient, Dish
+
+
+class ModelsTests(TestCase):
+    def test_dish_type_str(self):
+        dish_type = DishType.objects.create(name="test")
+        self.assertEqual(str(dish_type), dish_type.name)
+
+
+    def test_cook_str(self):
+        cook = get_user_model().objects.create(
+            username="test",
+            password="test123",
+            first_name="test_first",
+            last_name="test_last",
+            years_of_experience=10,
+        )
+        self.assertEqual(
+            str(cook),
+            f"{cook.username} ({cook.first_name} {cook.last_name}) exp.{cook.years_of_experience}"
+        )
+
+    def test_ingredient_str(self):
+        ingredient = Ingredient.objects.create(name="test")
+        self.assertEqual(str(ingredient), ingredient.name)
+
+
+    def test_dish_str(self):
+        dish_type = DishType.objects.create(name="test")
+        dish = Dish.objects.create(
+            name="test",
+            description="test",
+            price=10,
+            dish_type=dish_type,
+        )
+        self.assertEqual(str(dish), f"{dish.name} - (dish type: {dish.dish_type.name})")
+
+
+    def test_create_cook_with_years_of_experience(self):
+        username = "test"
+        password = "test123"
+        years_of_experience = 20
+        cook = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            years_of_experience=years_of_experience,
+        )
+        self.assertEqual(cook.username, username)
+        self.assertEqual(cook.years_of_experience, years_of_experience)
+        self.assertTrue(cook.check_password(password))
